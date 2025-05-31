9bd3f5838309bb3778668316470acfafcada824b
shafi0313 <msh.shafiul@gmail.com>
2025-05-31 23:29:05 +0600

    Enhance admin interface for Post model and update list template styling

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "is_published", "created_at")
+    search_fields = ("title", "content")
+    list_filter = ("is_published", "created_at")
+
+    ordering = ('-created_at',)
+    prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'content', 'is_published')
+        }),
+        ('Metadata', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
